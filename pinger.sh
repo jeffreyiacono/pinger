3ff8ebeb57e6f9531238f0261cdc9e1aad97f0d6
Jeff Iacono <iacono@squareup.com>
2012-11-18 15:05:13 -0800

    Instructions for all the things

--- pinger.sh
+++ pinger.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-
+#
+# Commandline utility that pings a url until a 200 response is achieved ...
+# or it gets tired and quits (where "gets tired" is defined as trying 20 times,
+# pausing 4 seconds in between each try).
+#
+# Usage:
+#
+#   $ ./pinger [url]
+#
+# - If a 200 status is encountered, script exits 0 and prints a success message
+# - If a 301 status is encountered, script exits 1 and prints a notice to supply
+#   the new url
+# - Otherwise the script sleeps for 4 seconds and retries
+#
+# exit status of 1 and message of "failed" is returned if script is not able to
+# get a 200 response from the supplied URL after 20 tries.
 function usage {
   echo "Usage: $0 [url]"
 }
