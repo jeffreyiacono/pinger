0d7c86244b64a16f7e482f0ca080b97d755bd439
Jeff Iacono <iacono@squareup.com>
2012-12-01 09:46:53 -0800

    Quote strings in conditions and make spacing of ; consistent

--- pinger.sh
+++ pinger.sh
@@ -22,24 +22,24 @@ function usage {
   echo "Usage: $0 [url]"
 }
 
-if [ $# != 1 ]; then
+if [ $# != 1 ] ; then
   echo "Error: must pass a url to ping."
   usage
   exit 1
-elif [ $1 == '-h' ]; then
+elif [ "$1" == "-h" ] ; then
   usage
   exit 0
-elif [ $1 == '-v' ]; then
+elif [ "$1" == "-v" ] ; then
   echo "version 1.0"
   exit 0
 fi
 
-for (( i=0; i < 20; i++ )); do
+for (( i=0; i < 20; i++ )) ; do
   http_header_response=$(curl -Is $1 | head -n1)
-  if echo $http_header_response | grep -q "200 OK"; then
+  if echo $http_header_response | grep -q "200 OK" ; then
     echo "[$(date)] $1 responded with '200 OK' status after $((i + 1)) tries"
     exit 0
-  elif echo $http_header_response | grep -q "301 Moved Permanently"; then
+  elif echo $http_header_response | grep -q "301 Moved Permanently" ; then
     echo "permanently moved - supply new url"
     exit 1
   else
